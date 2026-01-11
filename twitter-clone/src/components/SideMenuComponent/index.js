57d033639067ab82d29eb5ce10ab8e8bc8676f25
Ayan Baryshev <ayanbaryshev02@gmail.com>
2026-01-11 17:41:46 +0500

    Add twitter icon

--- twitter-clone/src/components/SideMenuComponent/index.js
+++ twitter-clone/src/components/SideMenuComponent/index.js
@@ -1,7 +1,9 @@
+import './styles.css'
+
 const SideMenu = () => {
     return(
-        <div className="w-25">
-            <p>Side menu</p>
+        <div className="w-25 mt-3 mx-5">
+            <img src={require('./images/twitter.png')} className='logo-icon' />
         </div>
     )
 }
