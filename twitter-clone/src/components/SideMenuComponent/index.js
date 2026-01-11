980e44ba6f60b85025bc2e672e4e4dc1f5f9fbe3
Ayan Baryshev <ayanbaryshev02@gmail.com>
2026-01-11 17:50:55 +0500

    Add 'Home' icon

--- twitter-clone/src/components/SideMenuComponent/index.js
+++ twitter-clone/src/components/SideMenuComponent/index.js
@@ -1,9 +1,17 @@
 import './styles.css'
+import {HOME_SVG} from './images'
 
 const SideMenu = () => {
     return(
         <div className="w-25 mt-3 mx-5">
             <img src={require('./images/twitter.png')} className='logo-icon' />
+        <div className='d-flex'>
+            <div style={{width: 30, height: 30}}>
+                {HOME_SVG}
+                </div>
+            
+            <p className='mx-3' style={{fontSize: 18}}>Home</p>
+        </div>
         </div>
     )
 }
