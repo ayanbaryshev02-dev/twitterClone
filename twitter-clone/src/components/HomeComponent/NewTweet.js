ddfd4a14eac5859a95000ead7aa667d670e738a0
Ayan Baryshev <ayanbaryshev02@gmail.com>
2026-01-13 13:53:38 +0500

    refactor: improve Home structure

--- twitter-clone/src/components/HomeComponent/NewTweet.js
+++ twitter-clone/src/components/HomeComponent/NewTweet.js
@@ -5,6 +5,7 @@ const NewTweet = () => {
         <div className="p-3" style={{borderBottom: '2px solid whitesmoke'}}>
             <img src={PROFILE_IMG_PATH} style={{width: 50, borderRadius: 50}}/>
             <input placeholder="Whats?" style={{border: 'none', fontSize: 18, outline: 'none'}} className="mx-3" />
+
         </div>
     )
 }
--- twitter-clone/src/components/HomeComponent/index.js
+++ twitter-clone/src/components/HomeComponent/index.js
@@ -1,13 +1,61 @@
 import NewTweet from "./NewTweet"
 import TweetsList from "./TweetsList"
+import React from "react"
+import { KZ_IMG_PATH, NFACTORIAL_IMG_PATH } from "./images"
 
-const Home = () =>{
-    return(
-        <div className="w-50 nt-3">
-        <h5 className="mx-3">Home</h5>
-        <NewTweet />
-        <TweetsList />
-        </div>
-    )
+class Home extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            tweets: [
+        {
+            authorName: 'Free KZ today',
+            authorUserName: '@kz',
+            img: KZ_IMG_PATH,
+            content: "LOREFPOK: :FKfkfgj fkjgr",
+            replies: 200,
+            retweets: 1000,
+            likes: 500,
+        },
+        {
+            authorName: 'NFactorial',
+            authorUserName: '@nfactorial',
+            img: NFACTORIAL_IMG_PATH,
+            content: "Курсы курсы курсы",
+            replies: 200,
+            retweets: 1000,
+            likes: 500,
+        },
+    ]
+        }
+
+    }
+    render(){
+
+        const { tweets } = this.state;
+
+          return(
+            <div className="w-50 nt-3">         
+            <h5 className="mx-3">Home</h5>
+            <NewTweet />
+            <TweetsList tweets = {tweets}/>
+         </div>
+        )
+}
 }
 export default Home
+
+
+
+
+// const Home = () =>{
+//     return(
+//         <div className="w-50 nt-3">
+//         <h5 className="mx-3">Home</h5>
+//         <NewTweet />
+//         <TweetsList />
+//         </div>
+//     )
+// }
+// export default Home
