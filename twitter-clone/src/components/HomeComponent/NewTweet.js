971a9a8ba18337332df1c37c0ecfc58801a6ab90
Ayan Baryshev <ayanbaryshev02@gmail.com>
2026-01-13 13:58:33 +0500

    Add Tweet Button

--- twitter-clone/src/components/HomeComponent/NewTweet.js
+++ twitter-clone/src/components/HomeComponent/NewTweet.js
@@ -2,10 +2,12 @@ import { PROFILE_IMG_PATH } from "./images"
 
 const NewTweet = () => {
     return(
-        <div className="p-3" style={{borderBottom: '2px solid whitesmoke'}}>
-            <img src={PROFILE_IMG_PATH} style={{width: 50, borderRadius: 50}}/>
-            <input placeholder="Whats?" style={{border: 'none', fontSize: 18, outline: 'none'}} className="mx-3" />
-
+        <div className="p-3 d-flex flex-column" style={{borderBottom: '2px solid whitesmoke'}}>
+            <div>  
+                <img src={PROFILE_IMG_PATH} style={{width: 50, borderRadius: 50}}/>
+                <input placeholder="Whats?" style={{border: 'none', fontSize: 18, outline: 'none'}} className="mx-3" />
+            </div>
+            <button style={{alignSelf: 'flex-end', color: 'white', background: '#1D9BF0', borderRadius: 20, border: 'none',}}>Tweet</button>
         </div>
     )
 }
